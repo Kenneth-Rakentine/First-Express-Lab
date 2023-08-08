fe24dc1488dafc28c7ffa8f78acee90ff8f15f14
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-08 16:58:47 -0400

    Magic 8 Ball

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const magicResponses = require('./models/magicResponses')
 
 const app = express();
 
 const PORT = 3000
+
+function getRandomInt(min,max){
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
 
 
@@ -15,11 +22,14 @@ app.get('/greeting/:name', (req, res)=>{
 })
 
 
-app.get('/tip/:total/:tipPercentage', (req, res )=>{
-    res.send(`<h1> Expected Tip is: ${(req.params.total  *(req.params.tipPercentage / 100)) }s</h1>`)
+app.get('/tip/:total/:tipPercent', (req, res )=>{
+    res.send(`<h1> Expected Tip: ${(req.params.total  *(req.params.tipPercent / 100)) }</h1>`)
 })
 
-
+app.get('/magic/:question', (req, res )=>{
+    res.send(`<h1>QUESTION: </h1>${req.params.question}
+    <h1>${magicResponses[getRandomInt(0, magicResponses.length)]}</h1>`)
+})
 
 app.listen(PORT, (req, res )=>{
     console.log(`server live: port ${PORT}`)
