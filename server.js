adba25639c8b1cd3540b9000bb4dd09fcb23285b
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-08 16:34:44 -0400

    tip calc

--- server.js
+++ server.js
@@ -10,16 +10,17 @@ app.get('/greeting', (req, res) =>{
     res.send('<h1> Hello, Stranger </h1>')
 })
 
-app.get('./greeting/:name', (req, res)=>{
+app.get('/greeting/:name', (req, res)=>{
     res.send(`<h1> What's Up, ${req.params.name}!</h1>`)
 })
 
 
 app.get('/tip/:total/:tipPercentage', (req, res )=>{
-    res.send(`<h1> Expected Tip is: ${(req.params.total * (req.params.tipPercentage / 100)) }s</h1>`)
+    res.send(`<h1> Expected Tip is: ${(req.params.total  *(req.params.tipPercentage / 100)) }s</h1>`)
 })
 
-// app.get('/magic', (req, res )=>{
 
-// })
 
+app.listen(PORT, (req, res )=>{
+    console.log(`server live: port ${PORT}`)
+})
